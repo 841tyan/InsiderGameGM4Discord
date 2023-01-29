c6fa89857ffa71f63a35bea04430137a6afb112f
841tyan <123857793+841tyan@users.noreply.github.com>
2023-01-29 15:32:18 +0900

    Update insidergm.py

--- insidergm.py
+++ insidergm.py
@@ -36,7 +36,7 @@ answers = ['りんご','ごりら','ばなな']
 defaultAnswerset = 'default'
 
 # 自分のBotのアクセストークンに置き換えてください
-TOKEN = ''
+TOKEN = 'MTA2OTEyNjIzNDc5MDk1NzA3OA.GPgvFJ.wcxug4NHyHxQaf-tPrFAPuaXjoOKC6qW8TUWfE'
 
 # 接続に必要なオブジェクトを生成
 client = commands.Bot(command_prefix='/')
